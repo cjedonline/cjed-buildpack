ff33da50349a56b3fa4eaedea764552401bd78eb
cjedonline <cjed@cjed.net>
2015-05-14 17:34:05 +0100

    fix launch script

--- bin/launch.sh
+++ bin/launch.sh
@@ -14,7 +14,7 @@ export PATH=$JAVA_HOME/bin:$PATH
 echo "org.osgi.service.http.port=$VCAP_APP_PORT"
 echo "org.osgi.service.http.port=$VCAP_APP_PORT" > $KARAF_HOME/etc/org.ops4j.pax.web.cfg
 sed 's/^featuresBoot=.*$/&,webconsole/' $KARAF_HOME/etc/org.apache.karaf.features.cfg > $KARAF_HOME/karaf.features.cfg
-mv -f $KARAF_HOME/karaf.features.cfg$KARAF_HOME/etc/org.apache.karaf.features.cfg
+mv -f $KARAF_HOME/karaf.features.cfg $KARAF_HOME/etc/org.apache.karaf.features.cfg
 
 # start Karaf as background task ..
 sh $KARAF_HOME/bin/karaf
