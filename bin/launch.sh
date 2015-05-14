53012f95ed429d73d6a69a1ac976a565d6994065
cjedonline <cjed@cjed.net>
2015-05-14 14:09:34 +0100

    fix IP addresses

--- bin/launch.sh
+++ bin/launch.sh
@@ -9,6 +9,13 @@ export JAVA_HOME=$HOME/jdk
 export KARAF_HOME=$HOME/apache-karaf
 export PATH=$JAVA_HOME/bin:$PATH
 
+# configure Karaf webconsole to deploy and run on port 80
+
+echo "org.osgi.service.http.port=$VCAP_APP_PORT"
+echo "org.osgi.service.http.port=$VCAP_APP_PORT" > $KARAF_HOME/etc/org.ops4j.pax.web.cfg
+sed 's/^featuresBoot=.*$/&,webconsole/' $KARAF_HOME/etc/org.apache.karaf.features.cfg > $KARAF_HOME/karaf.features.cfg
+mv -f $KARAF_HOME/karaf.features.cfg$KARAF_HOME/etc/org.apache.karaf.features.cfg
+
 # start Karaf as background task ..
 $KARAF_HOME/bin/start
 
