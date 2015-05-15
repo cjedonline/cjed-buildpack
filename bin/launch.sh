4c9a9637dbbf1000e90f2b79fc76ddb19733bd63
cjedonline <cjed@cjed.net>
2015-05-15 09:24:55 +0100

    converted to servicemix

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,17 +5,17 @@ pwd
 ls -la $HOME
 
 # Create environment for Karaf execution
-export JAVA_HOME=$HOME/jdk
-export KARAF_HOME=$HOME/apache-karaf
+export JAVA_HOME=$HOME/jre
+export SERVICEMIX_HOME=$HOME/apache-servicemix
 export PATH=$JAVA_HOME/bin:$PATH
 
 # configure Karaf webconsole to deploy and run on port 80
 
 echo "org.osgi.service.http.port=$VCAP_APP_PORT"
-echo "org.osgi.service.http.port=$VCAP_APP_PORT" > $KARAF_HOME/etc/org.ops4j.pax.web.cfg
-sed 's/^featuresBoot=.*$/&,webconsole/' $KARAF_HOME/etc/org.apache.karaf.features.cfg > $KARAF_HOME/karaf.features.cfg
-mv -f $KARAF_HOME/karaf.features.cfg $KARAF_HOME/etc/org.apache.karaf.features.cfg
+echo "org.osgi.service.http.port=$VCAP_APP_PORT" > $SERVICEMIX_HOME/etc/org.ops4j.pax.web.cfg
+sed 's/^featuresBoot=.*$/&,webconsole/' $SERVICEMIX_HOME/etc/org.apache.karaf.features.cfg > $SERVICEMIX_HOME/karaf.features.cfg
+mv -f $SERVICEMIX_HOME/karaf.features.cfg $SERVICEMIX_HOME/etc/org.apache.karaf.features.cfg
 
-# start Karaf as background task ..
-sh $KARAF_HOME/bin/karaf
+# start servicemix ..
+sh $SERVICEMIX_HOME/bin/servicemix
 
