a6eaa4e6a5c50ec2701da8890d7ff23fd588b9d6
cjedonline <cjed@cjed.net>
2015-06-03 14:21:12 +0100

    switch from servicemix to Karaf

--- bin/launch.sh
+++ bin/launch.sh
@@ -12,17 +12,21 @@ export BPM_PLUGINS=$HOME/bpm-plugins
 
 echo "Starting app with launch.sh"
 echo "CFBPM_PROPS = $CFBPM_PROPS"
-grep "^features" $KARAF_HOME/karaf.features.cfg
+grep "^features" $KARAF_HOME/etc/org.apache.karaf.features.cfg
 
 # configure Karaf webconsole to deploy and run on port 80
 
-sed "s#\(^.*\)file:.*\/plugins\/\(.*\)#\1file:$BPM_PLUGINS\/\2#g" bpmmicroservice.xml > $KARAF_HOME/deploy/bpmmicroservice.xml
-cat $KARAF_HOME/deploy/bpmmicroservice.xml
+# sed "s#\(^.*\)file:.*\/plugins\/\(.*\)#\1file:$BPM_PLUGINS\/\2#g" bpmmicroservice.xml > $KARAF_HOME/deploy/bpmmicroservice.xml
+# cat $KARAF_HOME/deploy/bpmmicroservice.xml
 
 echo "org.osgi.service.http.port=$VCAP_APP_PORT"
 echo "org.osgi.service.http.port=$VCAP_APP_PORT" > $KARAF_HOME/etc/org.ops4j.pax.web.cfg
 sed 's/^featuresBoot=.*$/&,webconsole/' $KARAF_HOME/etc/org.apache.karaf.features.cfg > $KARAF_HOME/karaf.features.cfg
 mv -f $KARAF_HOME/karaf.features.cfg $KARAF_HOME/etc/org.apache.karaf.features.cfg
+
+echo "Starting app with launch.sh"
+echo "CFBPM_PROPS = $CFBPM_PROPS"
+grep "^features" $KARAF_HOME/etc/org.apache.karaf.features.cfg
 
 # start Karaf ..
 sh $KARAF_HOME/bin/karaf
