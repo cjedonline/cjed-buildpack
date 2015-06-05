bc1e02741045818d79ad3b21af0ff6e3864fbee8
Charles Durrant <cdurrant@tibco.com>
2015-06-05 09:05:46 +0100

    Move to app defined by kar file

--- bin/launch.sh
+++ bin/launch.sh
@@ -8,7 +8,7 @@ ls -la $HOME
 export JAVA_HOME=$HOME/jre
 export KARAF_HOME=$HOME/karaf
 export PATH=$JAVA_HOME/bin:$PATH
-export BPM_PLUGINS=$HOME/bpm-plugins
+# export BPM_PLUGINS=$HOME/bpm-plugins
 
 # configure Karaf webconsole to deploy and run on specified CloudFoundry port
 
@@ -18,9 +18,9 @@ echo "org.osgi.service.http.port=$VCAP_APP_PORT" > $KARAF_HOME/etc/org.ops4j.pax
 # grep "featuresBoot" $KARAF_HOME/karaf.features.cfg
 # mv -f $KARAF_HOME/karaf.features.cfg $KARAF_HOME/etc/org.apache.karaf.features.cfg
 
-cp bpm-cloud-tpcl.xml $KARAF_HOME/deploy/bpm-cloud-tpcl.xml
-sed "s#\(^.*\)file:.*\/plugins\/\(.*\)#\1file:$BPM_PLUGINS\/\2#g" bpm-cloud-dac.xml > $KARAF_HOME/deploy/bpm-cloud-dac.xml
-cat $KARAF_HOME/deploy/bpm-cloud-dac.xml
+mv *.kar $KARAF_HOME/deploy
+# sed "s#\(^.*\)file:.*\/plugins\/\(.*\)#\1file:$BPM_PLUGINS\/\2#g" bpm-cloud-dac.xml > $KARAF_HOME/deploy/bpm-cloud-dac.xml
+# cat $KARAF_HOME/deploy/bpm-cloud-dac.xml
 
 echo "Starting app with launch.sh"
 echo "CFBPM_PROPS = $CFBPM_PROPS"
